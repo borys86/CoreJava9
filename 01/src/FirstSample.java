e515b159b2a6135b84eccaaef20c835063cabe25
borys86 <borys86@gmail.com>
2013-03-25 10:04:20 +0100

    test1

---
+++ 01/src/FirstSample.java
@@ -0,0 +1,19 @@
+public class FirstSample
+{
+
+	public static void main(String[] args)
+	{
+		{
+			System.out.println("Static inintialization block");
+			System.out.println("print this first");
+			System.out.println("blabla");
+		}
+
+		System.out.println("We will not use 'Hello Wordl'");
+
+		if (true) {
+			// /
+		}
+	}
+
+}
